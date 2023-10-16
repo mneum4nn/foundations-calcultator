9423da1a885c2686b3cbf6184f55649ac19a761f
mneum4nn <n3um4nn@proton.me>
2023-10-16 15:24:36 -0600

    Create sum,subtract,multiplication and division functions

---
+++ script.js
@@ -0,0 +1,14 @@
+let num1, num2, operator;
+
+function sum (x,y){
+    return x+y;
+}
+function subtract (x,y){
+    return x-y;
+}
+function multiply (x,y){
+    return x*y;
+}
+function divide (x,y){
+    return x/y;
+}
