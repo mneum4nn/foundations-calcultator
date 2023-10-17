29eb6014779d7b67de11e7e416675a9d25020af7
mneum4nn <n3um4nn@proton.me>
2023-10-17 14:39:17 -0600

    Create event listener to handle numerical input via buttons

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let num1, num2, operator;
+const operatorBtns = document.querySelectorAll('.operator');
+const numberBtns = document.querySelectorAll('.numbtn');
+const equalsBtn = document.querySelector('#equals');
+const display = document.querySelector('.output');
+
+let operator='';
+let prevVal='';
+let curVal ='';
+
+numberBtns.forEach((number)=> number.addEventListener('click', function(e){
+    handleNumber(e.target.textContent); 
+    display.textContent = curVal ;
+}))
+function handleNumber(num){
+    curVal+=num;
+}
 
 function sum (x,y){
     return x+y;
@@ -12,3 +27,24 @@ function multiply (x,y){
 function divide (x,y){
     return x/y;
 }
+
+function operate (operator, aNumber, anotherNumber) {
+    let result;
+    switch (operator){
+        case '+':
+            result = sum(aNumber,anotherNumber);
+            break;
+        case '-':
+            result = subtract(aNumber,anotherNumber);
+            break;
+        case '*':
+            result = multiply(aNumber,anotherNumber);
+            break;
+        case '/':
+            result = divide(aNumber,anotherNumber);
+            break;    
+    }
+    return result;
+}
+
+
