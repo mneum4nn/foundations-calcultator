13d83f0c3ab8d9ccd1aaaf0139c2b325c7572460
mneum4nn <n3um4nn@proton.me>
2023-10-17 17:14:18 -0600

    Include button to clear ongoing and previous operatios n s

--- script.js
+++ script.js
@@ -2,19 +2,35 @@ const operatorBtns = document.querySelectorAll('.operator');
 const numberBtns = document.querySelectorAll('.numbtn');
 const equalsBtn = document.querySelector('#equals');
 const display = document.querySelector('.output');
-
+const bigNumbers = document.querySelector('.big');
+const lilNumbers = document.querySelector('.small');
+const clearBtn = document.querySelector('#clear')
 let operator='';
 let prevVal='';
 let curVal ='';
 
 numberBtns.forEach((number)=> number.addEventListener('click', function(e){
     handleNumber(e.target.textContent); 
-    display.textContent = curVal ;
-}))
+    bigNumbers.textContent = curVal ;
+}));
+
 function handleNumber(num){
     curVal+=num;
 }
 
+clearBtn.addEventListener('click',(event)=> clearScreen());
+
+function clearScreen(){
+    bigNumbers.textContent="";
+    lilNumbers.textContent="";
+    curVal='';
+}
+function newLine(){
+    prevVal=curVal;
+    small.textContent=prevVal;
+    curVal='';
+    big.textContent=curVal;
+}
 function sum (x,y){
     return x+y;
 }
