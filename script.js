a05d45855b524bc13a9195ef1c76a265a8855e45
mneum4nn <n3um4nn@proton.me>
2023-10-18 16:09:49 -0600

    Create event listeners for operator and number buttons and accurately update the display value

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 const operatorBtns = document.querySelectorAll('.operator');
+
 const numberBtns = document.querySelectorAll('.numbtn');
-const equalsBtn = document.querySelector('#equals');
+
 const display = document.querySelector('.output');
 const bigNumbers = document.querySelector('.big');
 const lilNumbers = document.querySelector('.small');
+
 const clearBtn = document.querySelector('#clear')
+const equalsBtn = document.querySelector('#equals');
+
 let operator='';
 let prevVal='';
 let curVal ='';
+let num1=''; 
+let num2='';
 
-numberBtns.forEach((number)=> number.addEventListener('click', function(e){
-    handleNumber(e.target.textContent); 
-    bigNumbers.textContent = curVal ;
-}));
+numberBtns.forEach((number)=>{
+    number.addEventListener('click', function(e){
+        handleNumber(e.target.textContent);
+        bigNumbers.textContent = curVal;
+    });
+});
+
 
 function handleNumber(num){
     curVal+=num;
+    if (operator != ''){
+        num2 += num;
+    } 
+    else {
+        num1+=num;
+    }
+
 }
 
 clearBtn.addEventListener('click',(event)=> clearScreen());
@@ -24,13 +40,29 @@ function clearScreen(){
     bigNumbers.textContent="";
     lilNumbers.textContent="";
     curVal='';
+    num1='';
+    num2='';
+    operator='';
 }
-function newLine(){
-    prevVal=curVal;
-    small.textContent=prevVal;
-    curVal='';
-    big.textContent=curVal;
-}
+operatorBtns.forEach((opp) => {
+    opp.addEventListener('click', function(e){
+        operator = e.target.id;
+        curVal+=operator;
+        bigNumbers.textContent=curVal;
+    });
+});
+
+
+equalsBtn.addEventListener('click',(event)=>{
+    prevVal = curVal;
+    lilNumbers.textContent = prevVal;
+    curVal = operate(operator,num1,num2);
+    bigNumbers.textContent = curVal;
+    operator = '';
+    num1 = '';
+    num2 = '';
+})
+
 function sum (x,y){
     return x+y;
 }
